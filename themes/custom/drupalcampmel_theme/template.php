bb7560edcfe52bc116b6e9b1e31c739d24db9474
Stuart Clark <stu@rtclark.net>
2014-10-09 11:26:44 +1100

    Typography fix and colourisation.

--- themes/custom/drupalcampmel_theme/template.php
+++ themes/custom/drupalcampmel_theme/template.php
@@ -8,7 +8,19 @@
  * Implements hook_preprocess_panels_pane().
  */
 function drupalcampmel_theme_preprocess_panels_pane(&$vars) {
-  if ($vars['pane']->type == 'page_logo') {
+  // Use SVG logo.
+  if ('page_logo' == $vars['pane']->type) {
     $vars['content'] = str_replace('.png', '.svg', $vars['content']);
+  }
+
+  // Add SPANs to site name string.
+  if ('page_site_name' == $vars['pane']->type) {
+    preg_match_all('/([A-Z][a-z]+|[0-9]+)/', $vars['content'], $parts);
+
+    $vars['content'] = '';
+    foreach ($parts[1] as $part) {
+      $delta = strtolower($part);
+      $vars['content'] .= "<span class='part-{$delta}'>{$part}</span>";
+    }
   }
 }

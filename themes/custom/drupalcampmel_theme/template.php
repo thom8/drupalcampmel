99e456167d465f2071d0a4e5809c0ece3f0524a0
Stuart Clark <stu@rtclark.net>
2015-11-24 17:04:33 +1100

    Added Lunch and Break to schedule.

--- themes/custom/drupalcampmel_theme/template.php
+++ themes/custom/drupalcampmel_theme/template.php
@@ -135,30 +135,59 @@ function drupalcampmel_theme_preprocess_views_view_matrix(&$vars) {
   // Schedule.
   if (count($vars['rows']) <= 6) {
     $schedule = array(
-      1 => '10:45 - 11:30',
-      2 => '11:40 - 12:25',
-      3 => '13:10 - 13:55',
-      4 => '14:05 - 14:50',
-      5 => '15:20 - 16:05',
-      6 => '16:15 - 17:00',
+      1       => '10:45 - 11:30',
+      2       => '11:40 - 12:25',
+      'lunch' => t('Lunch'),
+      3       => '13:10 - 13:55',
+      4       => '14:05 - 14:50',
+      'break' => t('Break'),
+      5       => '15:20 - 16:05',
+      6       => '16:15 - 17:00',
     );
   }
   else {
     $schedule = array(
-      1 => '10:45 - 11:30',
-      2 => '11:35 - 12:05',
-      3 => '12:55 - 13:25',
-      4 => '13:30 - 14:00',
-      5 => '14:05 - 14:35',
-      6 => '15:00 - 15:30',
-      7 => '15:35 - 16:05',
-      8 => '16:10 - 16:40',
+      1       => '10:45 - 11:30',
+      2       => '11:35 - 12:05',
+      'lunch' => t('Lunch'),
+      3       => '12:55 - 13:25',
+      4       => '13:30 - 14:00',
+      5       => '14:05 - 14:35',
+      'break' => t('Break'),
+      6       => '15:00 - 15:30',
+      7       => '15:35 - 16:05',
+      8       => '16:10 - 16:40',
     );
   }
 
   $i = 0;
+  $next = array_keys($schedule);
   foreach ($vars['rows'] as $id => $row) {
     $i++;
     $vars['rows'][$id]['header']['data'] = $schedule[$i];
+    if (isset($next[$i]) && !is_numeric($next[$i])) {
+      $vars['rows'] = array_slice($vars['rows'], 0, $i, TRUE) +
+        array(
+          $next[$i] => array(
+            'header' => array(
+              'data'       => $schedule[$next[$i]],
+              'attributes' => array(),
+            ),
+            array(
+              'data'       => '',
+              'attributes' => array(),
+            ),
+            array(
+              'data'       => '',
+              'attributes' => array(),
+            ),
+            array(
+              'data'       => '',
+              'attributes' => array(),
+            ),
+          )
+        ) + array_slice($vars['rows'], $i, NULL, TRUE);
+      $vars['row_classes'][$next[$i]] = array();
+    }
   }
 }
